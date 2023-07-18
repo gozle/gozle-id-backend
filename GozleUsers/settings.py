b444e0e530537ace6a7b9246626d5579c94af655
ArslanRejepow <arslanrejepow223@gmail.com>
2023-07-18 13:30:07 +0500

    Simple JWTAuthentication

--- GozleUsers/settings.py
+++ GozleUsers/settings.py
@@ -168,9 +168,9 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'errors',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
