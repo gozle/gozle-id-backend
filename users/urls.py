629c82c5efbb25e6f3c59f1cba74b6a9dc24c3a0
ArslanRejepow <arslanrejepow223@gmail.com>
2023-07-23 23:31:59 +0500

    Add get_token to urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # from users.user_service import UserService
 
 from users.views import enterCard, forgotPassword, history, sign_up, update, verify_number, get_user, tfa, resource, \
-    get_client, oauth_login, register_payment, perform_payment, get_payment, accept_payment
+    get_client, oauth_login, register_payment, perform_payment, get_payment, accept_payment, get_token
 from users.views import register_order, order_status
 from users.views import transfer_request, transfer_verify
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('resource', resource),
     path('get/client/', get_client),
     path('login/', oauth_login),
+    path('get-oauth-token', get_token),
     path('payment/register', register_payment),
     path('payment/perform', perform_payment),
     path('payment/get', get_payment),
