4c188b4c72629aace389791b92e6d0f22feeeb69
ArslanRejepow <arslanrejepow223@gmail.com>
2023-07-24 12:24:05 +0500

    Change POST to GET on balance_history.py

--- users/views/balance_history.py
+++ users/views/balance_history.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from users.serializers import LoginSerializer, HistorySerializer
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @csrf_exempt
 def history(request, action):
     # Select action
