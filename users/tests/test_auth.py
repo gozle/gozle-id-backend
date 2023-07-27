aceef77ffd17a2787c1ab4f2c02d4d06f54387e2
ArslanRejepow <arslanrejepow223@gmail.com>
2023-07-28 01:19:15 +0500

    Test Get user and refresh token

--- users/tests/test_auth.py
+++ users/tests/test_auth.py
@@ -8,14 +8,31 @@ class AuthTest(APITestCase):
         ReservePhoneNumber.objects.create(user=self.user, phone_number='+99361000000')
 
     def test_sign_up_success(self):
+        # Test Sign-Up
         response = self.client.post('/api/auth/sign-up', {'phone_number': '+99361945186', }, format='json')
         self.assertEqual(response.status_code, 200)
 
+        # Test Verification with code
         user = User.objects.get(phone_number='+99361945186')
         verification_code = Verification.objects.get(user=user).code
         response = self.client.post('/api/auth/verify', {'phone_number': '+99361945186',
                                                          "verification-code": verification_code})
         self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", response.data.json())
+        self.assertIn("refresh_token", response.data.json())
+
+        refresh_token = response.data.json()['refresh_token']
+
+        # Test Get User
+        response = self.client.get('/api/auth/get-user',
+                                   headers={"Authorization": f"Bearer {response.data.json()['access_token']}"})
+        self.assertEqual(response.status_code, 200)
+
+        # Test Refresh Token
+        response = self.client.post('/api/token/refresh/', {'refresh_token': refresh_token}, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", response.data.json())
+        self.assertIn("refresh_token", response.data.json())
 
     def test_sign_up_blank(self):
         response = self.client.post('/api/auth/sign-up', {'phone_number': '', })
