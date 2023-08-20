334b1b9cd4b102c66fc9a5faf70a15fa54b9c4d1
ArslanRejepow <arslanrejepow223@gmail.com>
2023-08-20 20:28:47 +0500

    Get cities with region id

--- users/views/get_cities.py
+++ users/views/get_cities.py
@@ -3,14 +3,33 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import status
 
-from users.models import City
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+from users.models import City, Region
 from users.serializers import CitySerializer
 
+
+@swagger_auto_schema(method='get',
+                     manual_parameters=[
+                         openapi.Parameter("region", openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True,
+                                           description="Id of Region object"),
+                     ],
+                     responses={200: "Cities"}
+                     )
 @api_view(["GET"])
 @permission_classes([AllowAny])
 @csrf_exempt
 def get_cities(request):
-    regions = City.objects.all()
-    serializer = CitySerializer(regions, many=True)
+    # Get data
+    region_id = request.GET.get("region")
+
+    try:
+        region = Region.objects.get(id=region_id)
+    except:
+        return Response({"message": "Region not found"}, status=status.HTTP_404_NOT_FOUND)
+    cities = region.cities.all()
+    serializer = CitySerializer(cities, many=True)
     return Response(serializer.data)
