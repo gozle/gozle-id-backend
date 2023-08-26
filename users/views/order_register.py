97a1964244e9e19f645e61bec13b1ab73abe3ad8
ArslanRejepow <arslanrejepow223@gmail.com>
2023-08-26 19:26:59 +0500

    return not 400 error

--- users/views/order_register.py
+++ users/views/order_register.py
@@ -90,7 +90,7 @@ def register_order(request):
     # Get the response
     response_data = response.json()
 
-    if response_data.get("errorCode") != 0:
+    if response_data.get("errorCode"):
         return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
     order.order_id = response_data['orderId']
