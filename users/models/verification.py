3c0e9805babc4a72b13fa42e673bdec57edad91a
Arslan Rejepow <arslanrejepow223@gmail.co>
2023-09-08 20:49:37 +0500

    Fix import

--- users/models/verification.py
+++ users/models/verification.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-from .user import User
-
 
 class Verification(models.Model):
     code = models.IntegerField()
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='verification')
+        "users.User", on_delete=models.CASCADE, related_name='verification')
     type = models.CharField(max_length=10, default="phone", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
