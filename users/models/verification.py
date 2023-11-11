dc7b1a676b2ee3b6326de42a259149374d07aefa
begench <begenchallakov070@gmail.com>
2023-11-11 22:17:51 +0500

    some changes

--- users/models/verification.py
+++ users/models/verification.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Verification(models.Model):
     code = models.IntegerField()
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name='verification')
     type = models.CharField(max_length=10, default="phone", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
