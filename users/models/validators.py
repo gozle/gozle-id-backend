8db02ea080fa74293f10b9b6f64849a89448f54d
begench <begenchallakov070@gmail.com>
2023-11-16 21:03:54 +0500

    some changes

--- users/models/validators.py
+++ users/models/validators.py
@@ -1,11 +1,13 @@
 import re
-
+import emoji
 from django.core.exceptions import ValidationError
 
 
 # Function to validate name
 def validate_names(value):
     pattern = r'^[\w\s]+$'
+    if emoji.emoji_count(value):
+        raise ValidationError('Name cannot contain emojis')
     if not re.match(pattern, value):
         raise ValidationError('Name can contain only letters and space')
     if re.search(r'\d', value):
